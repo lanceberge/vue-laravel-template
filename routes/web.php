1a9ab4a3ffbf0a1c2a72fa043fa7f88c1faf1d8f
lanceberge <bergeron.lance6@gmail.com>
2025-01-09 19:17:50 -0500

    remove auth on main

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WaitListController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', []);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    return Inertia::render('Welcome');
 });
 
 Route::post('/waitlist/store', [WaitListController::class, 'store'])->name('waitlist.store');
-
-require __DIR__.'/auth.php';
