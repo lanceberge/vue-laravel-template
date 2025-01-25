670d338101b098fbe592a97c4ac556f2876b16a2
lanceberge <bergeron.lance6@gmail.com>
2025-01-25 12:54:34 -0500

    feat: notifications

--- resources/js/Layouts/DefaultPageLayout.vue
+++ resources/js/Layouts/DefaultPageLayout.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-  //
+  import { useNotifications } from '@/composables/useNotification'
+
+  const { notifications } = useNotifications()
 </script>
 
 <template>
@@ -8,13 +10,13 @@
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <slot />
       </div>
-    </div>
-  </main>
-
-  <main class="text-gray-800 dark:text-gray-200">
-    <div class="py-12">
-      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <slot />
+      <div class="fixed top-4 right-4 z-50 space-y-2">
+        <Notification
+          v-for="notification in notifications"
+          :key="notification.id"
+          :message="notification.message"
+          :type="notification.type"
+        />
       </div>
     </div>
   </main>
