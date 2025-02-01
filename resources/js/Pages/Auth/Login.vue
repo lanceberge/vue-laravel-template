2d99f503fa0dcdc09f6e7387acdd45d40eea1897
lanceberge <bergeron.lance6@gmail.com>
2025-02-01 10:29:16 -0500

    feat: link to register on login, LoginWithGoogle on login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,6 +6,7 @@
   import PrimaryButton from '@/Components/PrimaryButton.vue'
   import TextInput from '@/Components/TextInput.vue'
   import { Head, Link, useForm, usePage } from '@inertiajs/vue3'
+  import LoginWithGoogle from '@/Components/Auth/LoginWithGoogle.vue'
 
   defineProps<{
     canResetPassword?: boolean
@@ -76,7 +77,14 @@
         </label>
       </div>
 
-      <div class="mt-4 flex items-center justify-end">
+      <div class="mt-4 flex items-center justify-between">
+        <Link
+          :href="route('register')"
+          class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+        >
+          Not registered?
+        </Link>
+
         <Link
           v-if="canResetPassword"
           :href="route('password.request')"
@@ -93,6 +101,7 @@
           Log in
         </PrimaryButton>
       </div>
+      <LoginWithGoogle />
     </form>
   </GuestLayout>
 </template>
