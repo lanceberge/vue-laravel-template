319999f1c2917d5580bc9cc863dd3a0d16ab2e22
lanceberge <bergeron.lance6@gmail.com>
2025-02-01 10:29:02 -0500

    refactor: LoginWithGoogle component

---
+++ resources/js/Components/Auth/LoginWithGoogle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import GoogleIcon from '@/Icons/GoogleIcon.vue'
+</script>
+
+<template>
+  <div class="relative my-6">
+    <div class="absolute inset-0 flex items-center">
+      <div class="w-full border-t border-gray-300"></div>
+    </div>
+    <div class="relative flex justify-center text-sm">
+      <span class="bg-white px-2 text-gray-500">or</span>
+    </div>
+  </div>
+
+  <a :href="route('auth.google.redirect')" class="p-2 shadow-lg rounded-xl flex items-center mt-1"
+    ><GoogleIcon class="inline-block h-8 w-8" /><p class="ml-2">Continue with Google</p></a
+  >
+</template>
--- resources/js/Components/LoginWidget.vue
+++ resources/js/Components/LoginWidget.vue
@@ -3,7 +3,7 @@
   import InputLabel from './InputLabel.vue'
   import TextInput from './TextInput.vue'
   import InputError from './InputError.vue'
-  import GoogleIcon from '@/Icons/GoogleIcon.vue'
+  import LoginWithGoogle from './Auth/LoginWithGoogle.vue'
   import PrimaryButton from './PrimaryButton.vue'
 
   const form = useForm({
@@ -44,17 +44,7 @@
     >
       Get Started
     </PrimaryButton>
-  </form>
 
-  <div class="relative my-2">
-    <div class="absolute inset-0 flex items-center">
-      <div class="w-full border-t border-gray-300"></div>
-    </div>
-    <div class="relative flex justify-center text-sm">
-      <span class="bg-white px-2 text-gray-500">or</span>
-    </div>
-  </div>
-  <a :href="route('auth.google.redirect')" class="p-2 shadow-lg rounded-xl flex items-center mt-1"
-    ><GoogleIcon class="inline-block h-8 w-8" /><p class="ml-2">Continue with Google</p></a
-  >
+    <LoginWithGoogle />
+  </form>
 </template>
