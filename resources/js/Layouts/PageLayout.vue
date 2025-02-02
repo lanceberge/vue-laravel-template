e8e5215c0a3a663aeadce67207c84f3d753e9781
lanceberge <bergeron.lance6@gmail.com>
2025-02-02 13:43:12 -0500

    refactor: implement a PageLayout component

---
+++ resources/js/Layouts/PageLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import ContentLayout from './ContentLayout.vue'
+</script>
+
+<template>
+  <div
+    class="h-screen bg-gradient-to-br from-[hsl(var(--accent))] to-[hsl(var(--primary))] flex flex-col overflow-y-auto"
+  >
+    <slot name="nav-bar"> </slot>
+
+    <!-- Page Heading -->
+    <header class="bg-white shadow dark:bg-gray-800" v-if="$slots.header">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <slot name="header" />
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <ContentLayout class="bg-muted">
+      <slot />
+    </ContentLayout>
+  </div>
+</template>
