43b4262d26a7cb8b910b8d881948d0dea92d6212
lanceberge <bergeron.lance6@gmail.com>
2025-02-03 18:17:58 -0500

    add support email component

---
+++ resources/js/Components/SupportEmail.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { usePage } from '@inertiajs/vue3'
+
+  const page = usePage()
+  const supportEmail = `support@${page.props.appUrl}`
+</script>
+
+<template>
+  <a class="underline" :href="`mailto:${supportEmail}`">{{ supportEmail }}</a>
+</template>
