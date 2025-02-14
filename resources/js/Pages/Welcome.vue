b14766e001a0caf91373d3c264db4b59519259a0
lanceberge <bergeron.lance6@gmail.com>
2025-02-14 15:51:51 -0500

    full_saas branch

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,82 +1,49 @@
 <script setup lang="ts">
-  import Accordion from '@/Components/Accordion.vue'
+  import WelcomeLayout from '@/Layouts/WelcomeLayout.vue'
   import { Button } from '@/Components/shadcn/ui/button'
   import { Input } from '@/Components/shadcn/ui/input'
-  import SupportFooter from '@/Components/SupportFooter.vue'
-  import WelcomeLayout from '@/Layouts/WelcomeLayout.vue'
-  import { useForm } from '@inertiajs/vue3'
+  import { Link, useForm } from '@inertiajs/vue3'
+  import FrequentlyAskedQuestions from '@/Components/FrequentlyAskedQuestions.vue'
   import { ref } from 'vue'
-  import BillingPlans from './Billing/BillingPlans.vue'
+  import SupportFooter from '@/Components/SupportFooter.vue'
+  import LoginWidget from '@/Components/LoginWidget.vue'
 
-  const form = useForm({
-    email: '',
-  })
+  const form = useForm({})
 
-  const showForm = ref(true)
+  const topic = ref('')
+  const error = ref('')
 
   const submit = () => {
-    form.post(route('waitlist.store'), {
-      onSuccess: () => (showForm.value = false),
+    form.get(route('welcome.flashcards.create', { topic: topic.value }), {
+      onError: errors => {
+        error.value = errors.error
+      },
     })
   }
 </script>
 
 <template>
-  <WelcomeLayout>
-    <FrequentlyAskedQuestions />
+  <WelcomeLayout class="pb-20">
+    <!-- Header section -->
+    <section class="flex flex-col md:flex-row justify-around mt-14">
+      <div class="max-w-xl text-xl"> </div>
 
-    <div
-      class="fixed bottom-20 lg:bottom-40 left-1/2 -translate-x-1/2 border-2 rounded-xl border-gray-500 p-1 dark:bg-gray-900 w-fit max-w-[90%] w-[25rem]"
-    >
-      <Transition name="slide">
-        <div v-if="showForm">
-          <div v-if="form.errors.email" class="text-red-500 text-sm p-2">
-            Please enter a valid email address
-          </div>
-          <div class="flex justify-between items-center w-full space-x-2">
-            <Input
-              v-model="form.email"
-              @keyup.enter="submit"
-              autocomplete="email"
-              placeholder="Email Address"
-              class="w-full h-10 border-0 rounded-lg focus:border-0 focus:ring-0 rounded-lg"
-            >
-            </Input>
-            <Button
-              class="w-1/3 font-normal normal-case px-0 justify-center"
-              @click="submit"
-              :disbled="form.processing"
-              >Join Waitlist</Button
-            >
-          </div>
+      <div class="mt-6 md:mt-0 flex flex-col justify-end">
+        <div v-if="$page.props.auth.subscribed" class="flex flex-col space-y-2">
+          <p class="text-xl">You're logged in!</p>
+          <Link :href="route('dashboard')">
+            <Button variant="destructive" class="text-xl">Dashboard</Button>
+          </Link>
         </div>
-        <p v-else class="text-center w-full p-3"
-          >We've added {{ form.email }} to the wait list! We'll email you with updates</p
-        >
-      </Transition>
-    </div>
-
-    <BillingPlans />
+        <Link :href="route('billing')" v-else-if="$page.props.auth.user" class="mt-auto">
+          <Button variant="destructive" class="text-xl">Subscribe Now</Button>
+        </Link>
+        <LoginWidget v-else />
+      </div>
+    </section>
 
     <template #footer>
       <SupportFooter />
     </template>
   </WelcomeLayout>
 </template>
-
-<style scoped>
-  .slide-enter-active,
-  .slide-leave-active {
-    transition: all 0.5s ease;
-  }
-
-  .slide-enter-from {
-    transform: translateY(100%);
-    opacity: 0;
-  }
-
-  .slide-leave-to {
-    transform: translateY(-100%);
-    opacity: 0;
-  }
-</style>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -10,6 +10,7 @@ export interface User {
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
   auth: {
     user: User
+    subscribed: boolean
   }
   ziggy: Config & { location: string }
   appName: string
