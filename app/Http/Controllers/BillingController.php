d8f0574121a0a18a9ee4d8a19be304f0fc449092
lanceberge <bergeron.lance6@gmail.com>
2025-03-12 18:26:08 -0400

    add support for 1 month free coupon codes

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class BillingController extends Controller
 {
     // TODO if not logged in, route to GoogleAuth with the stripe thing as a callback
-    public function subscribe(string $priceName, Request $request)
+    public function subscribe(string $priceName, Request $request, string $code = null)
     {
         $priceIds = config('services.stripe.price_ids');
 
@@ -17,14 +17,24 @@ class BillingController extends Controller
 
         $priceId = $priceIds[$priceName];
 
+        $trialDaysForCodes = ['1MONTHFREE' => 30];
+
+        if ($code !== null && isset($trialDaysForCodes[$code])) {
+            return $request->user()
+                ->newSubscription('default', $priceId)
+                ->trialDays($trialDaysForCodes[$code])
+                ->checkout([
+                    'success_url' => route('dashboard'),
+                    'cancel_url' => route('welcome'),
+                ]);
+        }
         return $request->user()
-          ->newSubscription('default', $priceId)
-          ->trialDays(5)
-          ->allowPromotionCodes()
-          ->checkout([
-            'success_url' => route('flashcards.create'),
-            'cancel_url' => route('welcome'),
-          ]);
+            ->newSubscription('default', $priceId)
+            ->allowPromotionCodes()
+            ->checkout([
+                'success_url' => route('dashboard'),
+                'cancel_url' => route('welcome'),
+            ]);
     }
 
     public function manage(Request $request)
--- resources/js/Pages/Billing/BillingPlans.vue
+++ resources/js/Pages/Billing/BillingPlans.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
   import BillingPlan from './BillingPlan.vue'
+  import { Input } from '@/Components/shadcn/ui/input'
+
+  const code = ref('')
 </script>
 
 <template>
   <div class="flex flex-col md:flex-row items-center justify-center md:space-x-20">
-    <BillingPlan :name="'Yearly'" :cost="10" :planName="'yearly_plan'">
+    <BillingPlan :name="'Yearly'" :cost="7.25" :planName="'yearly_plan'" :code>
       <template #inline-description>
         <div class="text-sm">billed annually</div>
         <div class="text-sm">6 months free</div>
       </template>
     </BillingPlan>
-    <BillingPlan :name="'Monthly'" :cost="20" :planName="'monthly_plan'" />
+    <BillingPlan :name="'Monthly'" :cost="14.5" :planName="'monthly_plan'" :code>
+      <template #inline-description>
+        <div class="text-sm">billed monthly</div>
+      </template>
+    </BillingPlan>
   </div>
+  <Input v-model="code" class="max-w-md mx-auto mt-5" placeholder="Input a coupon code" />
 </template>
